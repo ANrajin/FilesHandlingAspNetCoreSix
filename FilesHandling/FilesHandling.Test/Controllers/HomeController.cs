f3c39dbb6a62cf1d6ff0e273b15c8e8b1f23b32a
anrajin <an.rajin@gmail.com>
2022-06-09 16:28:21 +0600

    image upload using filepond js library

--- FilesHandling/FilesHandling.Test/Controllers/HomeController.cs
+++ FilesHandling/FilesHandling.Test/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace FilesHandling.Test.Controllers
             return View();
         }
 
+        public IActionResult FilePond()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
